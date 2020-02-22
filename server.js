ed5cfcadb75144d9f13a5cab12bf82108389f109
EricVincitore <epv1001@verizon.net>
2020-02-22 17:00:07 -0500

    heroku stuff

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.engine("handlebars", handlebars({defaultLayout:"main"}));
 app.set("view engine", "handlebars");
 
 //if deployed, use yhe deployed database. Otherwise use hte local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater"; //mongodb://user1:password1@ds117888.mlab.com:17888/heroku_78c10xl3";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:password1@ds117888.mlab.com:17888/heroku_78c10xl3";
 
 //connect to the mongo DB
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
